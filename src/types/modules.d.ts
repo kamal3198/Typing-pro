fdf754185eb39e54985b2534ea42dae0ac22ab23
kamal3198 <232p1a3198@gmail.com>
2026-02-01 20:54:10 +0530

    chore(types): declare .jsx page modules to remove TS7016 warnings

---
+++ src/types/modules.d.ts
@@ -0,0 +1,29 @@
+declare module './pages/Games' {
+  import React from 'react';
+  const Component: React.ComponentType<any>;
+  export default Component;
+}
+
+declare module './pages/History' {
+  import React from 'react';
+  const Component: React.ComponentType<any>;
+  export default Component;
+}
+
+declare module './pages/Settings' {
+  import React from 'react';
+  const Component: React.ComponentType<any>;
+  export default Component;
+}
+
+declare module './components/games/EnemyShooter' {
+  import React from 'react';
+  const Component: React.ComponentType<any>;
+  export default Component;
+}
+
+declare module './components/games/FallingWords' {
+  import React from 'react';
+  const Component: React.ComponentType<any>;
+  export default Component;
+}
